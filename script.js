5123c12439537ba2c3f31263aaefc1330fb3907e
julius-tamale <juliotamex@proton.me>
2022-12-16 13:58:26 +0200

    chrome update

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
+let myLeads = [];
 const inputEl = document.getElementById('input-el');
 const inputBtn = document.getElementById('input-btn');
-const deleteBtn = document.getElementById('delete-btn');
+//const deleteBtn = document.getElementById('delete-btn');
 const saveBtn = document.getElementById('save-btn');
+const ulEl = document.getElementById('ul-el');
 
-inputBtn.addEventListener('click', function(){
-
+inputBtn.addEventListener('click', function() {
+    myLeads.push(inputEl.value);
+    inputEl.value = '';
+    render();
 })
 
-deleteBtn.addEventListener('click', function(){
-
-})
-
-saveBtn.addEventListener('click', function(){
-
-})
+function render() {
+    let myLeadsLinks = '';
+    for(let i = 0; i < myLeads.length; i++) {
+        myLeadsLinks += `
+                        <li>
+                            <a href='${myLeads[i]}' target='_blank'>
+                                ${myLeads[i]}
+                            </a>
+                        </li>`;
+    }
+    ulEl.innerHTML = myLeadsLinks;
+}
