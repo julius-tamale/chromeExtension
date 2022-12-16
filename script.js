270d6090815ae6572ad13fef960c1f441fce0c1a
julius-tamale <juliotamex@proton.me>
2022-12-16 14:39:54 +0200

    chrome update

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 let myLeads = [];
 const inputEl = document.getElementById('input-el');
 const inputBtn = document.getElementById('input-btn');
-//const deleteBtn = document.getElementById('delete-btn');
+const deleteBtn = document.getElementById('delete-btn');
 const saveBtn = document.getElementById('save-btn');
 const ulEl = document.getElementById('ul-el');
+
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+if(leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage;
+    render();
+}
 
 inputBtn.addEventListener('click', function() {
     myLeads.push(inputEl.value);
     inputEl.value = '';
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
     render();
 })
 
@@ -23,3 +31,17 @@ function render() {
     }
     ulEl.innerHTML = myLeadsLinks;
 }
+
+deleteBtn.addEventListener('dblclick', function() {
+    localStorage.clear();
+    myLeads = [];
+    render();
+})
+
+saveBtn.addEventListener("click", function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function() {
+        myLeads.push(tabs[0].url);
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        render()
+    })
+})
