3480c0ae236254bb8e402d64f110d974ea686135
julius-tamale <juliotamex@proton.me>
2022-12-16 11:59:38 +0200

    js update

---
+++ script.js
@@ -0,0 +1,16 @@
+const inputEl = document.getElementById('input-el');
+const inputBtn = document.getElementById('input-btn');
+const deleteBtn = document.getElementById('delete-btn');
+const saveBtn = document.getElementById('save-btn');
+
+inputBtn.addEventListener('click', function(){
+
+})
+
+deleteBtn.addEventListener('click', function(){
+
+})
+
+saveBtn.addEventListener('click', function(){
+
+})
